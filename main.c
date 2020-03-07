5e3a7c47a2136e5ce82d41b0807a56892e85e110
Sarah Kwon <hakwon@umich.edu>
2020-03-06 20:06:07 -0500

    rgb_init

--- main.c
+++ main.c
@@ -9,3 +9,13 @@ int main()
     while (1) {
     }
 }
+
+void rgb_init() {
+    TCC0->WAVE.WAVEGEN = TCC_WAVE_WAVEGEN_NPWM;
+    TCC0->PER.reg = 0xFF; //255
+    TCC0->CTRLA.ENABLE = TCC_CTRLA_ENABLE;
+}
+
+void rgb_set() {
+
+}
